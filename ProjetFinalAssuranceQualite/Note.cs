c41fcdb42e979da3ec977b09aa71b4147f530a9d
tapsdam <82062282+tapsdam@users.noreply.github.com>
2021-04-16 14:07:51 -0400

    Update Note.cs

--- ProjetFinalAssuranceQualite/Note.cs
+++ ProjetFinalAssuranceQualite/Note.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,21 @@ namespace ProjetFinalAssuranceQualite
 {
 	class Note
 	{
+		public int NumeroEtudiant { get; set; }
+		public int NumeroCours { get; set; }
+		private double laNote;
+		public double LaNote { get { return laNote; } set { laNote = (value >= 0 && value <= 100) ? value : 0; } }
+
+		public Note(Etudiant etudiant, Cours cours, double laNote)
+		{
+			this.LaNote = laNote;
+			this.NumeroEtudiant = etudiant.NumeroEtudiant;
+			this.NumeroCours = cours.NumeroCours;
+		}
+
+		public override string ToString()
+		{
+			return "\tNote : " + this.LaNote;
+		}
 	}
 }
