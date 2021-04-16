270a760f221bb87c97dfcdf74eda61ad7f24db0f
BahDioulde <82293986+BahDioulde@users.noreply.github.com>
2021-04-16 14:44:39 -0400

    Update Program.cs
    Définition des listes Etudiant, Note et Cours, puis implémentation de la méthode permettant de vérifier la saisie des entiers.

--- ProjetFinalAssuranceQualite/Program.cs
+++ ProjetFinalAssuranceQualite/Program.cs
@@ -10,6 +10,33 @@ namespace ProjetFinalAssuranceQualite
 	{
 		static void Main(string[] args)
 		{
+		static Etudiant etudiant;
+		public static List<Etudiant> ListEtudiants = new List<Etudiant>();
+		public static List<Note> ListeNotesEtudiant = new List<Note>();
+		public static List<Cours> ListDesCours = new List<Cours>();
+		static int choixDuMenu;
+		static Cours cours;
+
+		public static int VerifierLaSaisieEntier()
+		{
+			do
+			{
+				int ValeurDeSortie = 0;
+				string valSaisi = Console.ReadLine();
+				if (int.TryParse(valSaisi, out ValeurDeSortie))
+				{
+					return ValeurDeSortie;
+				}
+				else
+				{
+					Console.ReadKey();
+					Console.Write("\tVous devez saisir un nombre : ");
+					Thread.Sleep(2000);
+					return 0;
+				}
+			} while (VerifierLaSaisieEntier() == 0);
+
+		}
 		}
 	}
 }
