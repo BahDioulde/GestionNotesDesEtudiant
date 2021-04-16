e9df78a9d89601ba8d123c450d353e3ffb03612f
tapsdam <82062282+tapsdam@users.noreply.github.com>
2021-04-15 21:31:55 -0400

    Update Cours.cs
    Définition d'une méthode RechercherCours permettant de rechercher un cours dans la liste des cours disponibles.

--- ProjetFinalAssuranceQualite/Cours.cs
+++ ProjetFinalAssuranceQualite/Cours.cs
@@ -8,5 +8,15 @@ namespace ProjetFinalAssuranceQualite
 {
 	class Cours
 	{
+		public static void RechercherCours(int numeroCoursRechercher)
+		{
+			foreach (Cours cours in Program.ListDesCours)
+			{
+				if (cours.NumeroCours == numeroCoursRechercher)
+				{
+					Console.WriteLine(cours.ToString());
+				}
+			}
+		}
 	}
 }
