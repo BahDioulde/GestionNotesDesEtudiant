f26fd3bbb4b019de275ca82b486981897aec5f33
Abdoulayez <82297389+Abdoulayez@users.noreply.github.com>
2021-04-16 14:53:03 -0400

    Update Program.cs
    Definition du menu de l'activité permettant à l'utilisateur de faire son choix de fonctions

--- ProjetFinalAssuranceQualite/Program.cs
+++ ProjetFinalAssuranceQualite/Program.cs
@@ -10,6 +10,20 @@ namespace ProjetFinalAssuranceQualite
 	{
 		static void Main(string[] args)
 		{
+			public static void Menu()
+			{
+				Console.Clear();
+				Console.WriteLine("1. Gestion des etudiants ");
+				Console.WriteLine("2. Enregistrement Cours");
+				Console.WriteLine("3. Enregistrement Notes");
+				Console.WriteLine("4. Afficher Etudiants ");
+				Console.WriteLine("5. Rechercher un releve");
+			do
+			{
+				choixDuMenu = VerifierLaSaisieEntier();
+			} while (choixDuMenu < 1);
+
+			}
 		}
 	}
 }
