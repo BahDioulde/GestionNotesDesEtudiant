82fde2fec343d8239c4dee0e2797e772372c6abb
BahDioulde <82293986+BahDioulde@users.noreply.github.com>
2021-04-15 21:54:04 -0400

    Update Etudiant.cs
    Définition de la méthode permettant l'affichage de la liste des étudiants de la classe.

--- ProjetFinalAssuranceQualite/Etudiant.cs
+++ ProjetFinalAssuranceQualite/Etudiant.cs
@@ -8,5 +8,16 @@ namespace ProjetFinalAssuranceQualite
 {
 	class Etudiant
 	{
+		public static void AfficherListeEtudiant(List<Etudiant> listeEtudiants)
+		{
+			if (listeEtudiants.Count < 1)
+			{
+				Console.WriteLine("Aucun etudiant disponible dans votre base de donnee\n");
+			}
+			foreach (Etudiant etud in listeEtudiants)
+			{
+				Console.WriteLine(etud.ToString());
+			}
+		}
 	}
 }
