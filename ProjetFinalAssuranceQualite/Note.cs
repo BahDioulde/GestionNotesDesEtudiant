f23709e009276057864064507ec47c52c81e1e2c
1HassanAbdiG <82282318+1HassanAbdiG@users.noreply.github.com>
2021-04-16 14:33:43 -0400

    Update Note.cs
    Méthode permettant d'attribuer un grade selon  sa moyenne.

--- ProjetFinalAssuranceQualite/Note.cs
+++ ProjetFinalAssuranceQualite/Note.cs
@@ -8,5 +8,21 @@ namespace ProjetFinalAssuranceQualite
 {
 	class Note
 	{
+		public static string AttribuerGrade()
+		{
+			string grade = "";
+			if (CalculerMoyenne() <= 54) grade = "EC";
+			else if (CalculerMoyenne() <= 59) grade = "DR";
+			else if (CalculerMoyenne() <= 63) grade = "C-";
+			else if (CalculerMoyenne() <= 66) grade = "C";
+			else if (CalculerMoyenne() <= 69) grade = "C+";
+			else if (CalculerMoyenne() <= 73) grade = "B-";
+			else if (CalculerMoyenne() <= 76) grade = "B";
+			else if (CalculerMoyenne() <= 79) grade = "B+";
+			else if (CalculerMoyenne() <= 84) grade = "A-";
+			else if (CalculerMoyenne() <= 89) grade = "A";
+			else if (CalculerMoyenne() <= 100) grade ="A+";
+			return grade;
+		}
 	}
 }
