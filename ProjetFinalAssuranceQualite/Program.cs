e672f3b626bc0af92072c5a2062cdb1d888d2976
1HassanAbdiG <82282318+1HassanAbdiG@users.noreply.github.com>
2021-04-16 15:00:19 -0400

    Update Program.cs
    Méthode permettant enregistrer un étudiant.

--- ProjetFinalAssuranceQualite/Program.cs
+++ ProjetFinalAssuranceQualite/Program.cs
@@ -10,6 +10,34 @@ namespace ProjetFinalAssuranceQualite
 	{
 		static void Main(string[] args)
 		{
+			public static void EnregistrerEtudiant()
+		{
+			
+			string reponseChoisie = "O";
+			int numeroEtudiant;
+			do
+			{
+				Console.Clear();
+                Console.WriteLine("\t***** Inscription des etudiants *****");
+				Console.Write("\n\tNumero de l'etudiant : ");
+				numeroEtudiant = VerifierLaSaisieEntier();
+				if (numeroEtudiant < 1)
+					continue;
+				Console.Write("\tNom de l'etudiant : ");
+				string nomEtudiant = Console.ReadLine();
+				Console.Write("\tPrenom de l'etudiant : ");
+				string prenomEtudiant = Console.ReadLine();
+
+				etudiant = new Etudiant(numeroEtudiant, nomEtudiant, prenomEtudiant);
+				ListEtudiants.Add(etudiant);
+				do
+				{
+					Console.Write("\tvoulez vous continuer L'ENREGISTREMENT O/N ? : ");
+					reponseChoisie = Console.ReadLine();
+				} while (reponseChoisie.ToUpper().Substring(0) != "N" && reponseChoisie.ToUpper().Substring(0) != "O");
+
+			} while (reponseChoisie.ToUpper().Substring(0) == "O");
+		}
 		}
 	}
 }
